0998c665fcf8fa7bde379da48875f46180a5a5c8
Chad <andchadtx@gmail.com>
2018-08-23 19:17:48 -0500

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,25 +1,36 @@
-//This makes the grid
-function makeGrid() {
-    var height = document.getElementById('input_height').value;
-    var width = document.getElementById('input_width').value;
-    var table = document.getElementById('pixel_canvas');
-	// Table grid creation code :
-    table.innerHTML = '';
-    var tbody = document.createElement('tbody');
-    for (var i = 0; i < height; i++) {
-        var tr = document.createElement('tr');
-        for (var j = 0; j < width; j++) {
-            var td = document.createElement('td');
-            td.appendChild(document.createTextNode(''));
-            tr.appendChild(td);
-        }
-        tbody.appendChild(tr);
+// Select color input
+let color = document.getElementById("colorPicker");
+ // Select size input
+let height = document.getElementById("input_height");
+let width = document.getElementById("input_width");
+let sizePicker = document.getElementById("sizePicker");
+ //Design canvas
+let canvas = document.getElementById("pixel_canvas");
+ // When size is submitted by the user, call makeGrid()
+ function makeGrid() {
+      for (let i=0; i < height.value; i++){
+          let tr = document.createElement("tr");
+          canvas.appendChild(tr);
+          for (let j=0; j < width.value; j++){
+              let td = document.createElement("td");
+              tr.appendChild(td);
+              td.addEventListener('click', function() {
+                td.style.backgroundColor = colorPicker.value
+        });
+      }
     }
-    table.appendChild(tbody);
-}
+  }
+   function clearGrid(){
+      while (canvas.firstChild){
+           canvas.removeChild(canvas.firstChild);
+      }
+  }
 
-//jQuery to change color of pixel when clicked.
-$('body').on('click', 'td', function() {
-	var color = document.getElementById('colorPicker').value;
-    $(this).css('background-color', color);
-});
+ //Event Listeners for changing color and size
+const colorPicker = document.getElementById('colorPicker');
+color = colorPicker.value;
+ sizePicker.onsubmit = function(event){
+  event.preventDefault();
+  clearGrid();
+  makeGrid();
+};
